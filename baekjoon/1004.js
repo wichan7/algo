44cf104e38ab928c54c1a0a48940305d197be8c8
wichan7 <wichan7@naver.com>
2023-08-06 01:27:28 +0900

    Solved: 1004 - 어린왕자

--- baekjoon/1004.js
+++ baekjoon/1004.js
@@ -19,7 +19,7 @@
  * r^2 > (x-a)^2 + (y-b)^2 이면 탈출카운트 += 1
  */
 
-class Circle {
+class Universe {
     constructor(x, y, r) {
         this.x = x;
         this.y = y;
@@ -27,6 +27,56 @@ class Circle {
     }
 }
 
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+function isOnUniverse(point, universe) {
+    const p = point;
+    const u = universe;
+
+    if ( Math.pow(p.x - u.x, 2) + Math.pow(p.y - u.y, 2) < Math.pow(u.r, 2) ) {
+        return true;
+    }
+
+    return false;
+}
+
 let line = require("fs").readFileSync("./1004.input", "utf8");
 //let line = require("fs").readFileSync("/dev/stdin", "utf8");
 let input = line.trim().replace(/\r/gi, "").split("\n");
+
+// define testcase amount
+const testCase = parseInt(input[0]);
+let cursor = 0;
+for (let a=0; a<testCase; a++) {
+    // define two points
+    cursor += 1;
+    const [x1, y1, x2, y2] = input[cursor].split(" ").map( e => parseInt(e) );
+    const start = new Point(x1, y1);
+    const dest = new Point(x2, y2);
+
+    // define count of universes
+    cursor += 1;
+    const [universesCnt] = input[cursor].split(" ").map( e => parseInt(e) );
+    
+    // push universe in universes
+    let universes = [];
+    for (let b=0; b<universesCnt; b++) {
+        cursor += 1;
+        const [x, y, r] = input[cursor].split(" ").map( e => parseInt(e) );
+        universes.push(new Universe(x, y, r));
+    }
+
+    let resultCount = 0;
+    for (let universe of universes) {
+        if ( isOnUniverse(start, universe) ^ isOnUniverse(dest, universe) ) {
+            resultCount += 1;
+        }
+    }
+    
+    console.log(resultCount);
+}
