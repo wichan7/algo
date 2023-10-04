cc37ce31bbcf08d5168b3025873bd6164ab8e6b5
wichan7 <wichan7@naver.com>
2023-10-04 14:48:36 +0900

    [BOJ] Gold5 평범한 배낭, KnapSack Algorithm

--- baekjoon/javascript/12865.js
+++ baekjoon/javascript/12865.js
@@ -1,73 +1,73 @@
 // https://www.acmicpc.net/problem/12865
+// KnapSack algorithm
 const stdin = (
     process.platform === "linux"
         ? require('fs').readFileSync("/dev/stdin").toString()
         : `
-        40 25
-        2 4
-        1 3
-        3 5
-        4 8
-        2 1
-        2 4
-        1 3
-        3 5
-        4 8
-        2 1
-        2 4
-        1 3
-        3 5
-        4 8
-        2 1
-        2 4
-        1 3
-        3 5
-        4 8
-        2 1
-        2 4
-        1 3
-        3 5
-        4 8
-        2 1
-        2 4
-        1 3
-        3 5
-        4 8
-        2 1
-        2 4
-        1 3
-        3 5
-        4 8
-        2 1
-        2 4
-        1 3
-        3 5
-        4 8
-        2 1
+        5 14
+        1 5
+        2 3
+        3 6
+        4 7
+        5 8
 `
-).trim().split("\n").map( e => e.trim() );
+).trim().split("\n").map(e => e.trim());
 
 // init
-const [N, K] = stdin[0].split(" ");
-const items = stdin.slice(1, N + 1).map( e => e.split(" ").map(el => Number(el)) );
-let max = 0;
-comb(0, 0, 0);
-console.log(max);
+const [N, K] = stdin[0].split(" ").map(e => Number(e));
+const items = stdin.slice(1, N + 1).map(e => e.split(" ").map(el => Number(el)));
+const memo = new Array(K + 1).fill(0);
 
-function comb(idx, wsum, vsum) {
-    let flag = true;
-    for (let i=idx; i<items.length; i++) {
-        let [W, V] = items[i];
-        if (wsum + W <= K) {
-            flag = false;
-            comb(i + 1, wsum + W, vsum + V);
+// knapsack
+for (let j = 0; j < items.length; j++) {
+    const [w, v] = items[j];
+    for (let i = K; i >= 0; i--) {
+        if (i + w <= K && memo[i + w] < memo[i] + v) {
+            memo[i + w] = memo[i] + v;
         }
-    }
-
-    if (flag) {
-        if (max < vsum) {
-            max = vsum;
-        }
-        return;
     }
 }
+
+let max = -Infinity;
+for (let i=0; i<memo.length; i++)
+    if (max < memo[i]) max = memo[i];
+
+console.log(max);
+
+// brute force
+// const stdin = (
+//     process.platform === "linux"
+//         ? require('fs').readFileSync("/dev/stdin").toString()
+//         : `
+//         4 7
+//         6 13
+//         4 8
+//         3 6
+//         5 12
+// `
+// ).trim().split("\n").map( e => e.trim() );
+
+// // init
+// const [N, K] = stdin[0].split(" ");
+// const items = stdin.slice(1, N + 1).map( e => e.split(" ").map(el => Number(el)) );
+// let max = 0;
+// comb(0, 0, 0);
+// console.log(max);
+
+// function comb(idx, wsum, vsum) {
+//     let flag = true;
+//     for (let i=idx; i<items.length; i++) {
+//         let [W, V] = items[i];
+//         if (wsum + W <= K) {
+//             flag = false;
+//             comb(i + 1, wsum + W, vsum + V);
+//         }
+//     }
+
+//     if (flag) {
+//         if (max < vsum) {
+//             max = vsum;
+//         }
+//         return;
+//     }
+// }
