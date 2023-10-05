49bd61804426b608b05d136ca9e4f8d5913de94f
wichan7 <wichan7@naver.com>
2023-10-05 15:14:22 +0900

    Refactor: 함수명 변경, heappop 필요없는 로직 삭제

--- practice/heap/heap.js
+++ practice/heap/heap.js
@@ -9,7 +9,7 @@ class MinHeap {
 
     swap = (a, b) => [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
 
-    heappush = (value) => {
+    push = (value) => {
         this.heap.push(value);
         let curIdx = this.heap.length - 1;
         let parIdx = (curIdx / 2) >> 0;
@@ -21,7 +21,7 @@ class MinHeap {
         }
     }
 
-    heappop = () => {
+    pop = () => {
         const min = this.heap[1];
         if (this.heap.length <= 2) this.heap = [null];
         else this.heap[1] = this.heap.pop();
@@ -29,14 +29,6 @@ class MinHeap {
         let curIdx = 1;
         let leftIdx = curIdx * 2;
         let rightIdx = curIdx * 2 + 1;
-
-        if (!this.heap[leftIdx]) return min;
-        if (!this.heap[rightIdx]) {
-            if (this.heap[leftIdx] < this.heap[curIdx]) {
-                this.swap(leftIdx, curIdx);
-            }
-            return min;
-        }
 
         while (this.heap[leftIdx] < this.heap[curIdx] || this.heap[rightIdx] < this.heap[curIdx]) {
             const minIdx = this.heap[leftIdx] > this.heap[rightIdx] ? rightIdx : leftIdx;
