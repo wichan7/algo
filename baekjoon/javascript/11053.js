3aad05e34bf7852b552a035ae54953c4e743c4da
wichan7 <wichan7@naver.com>
2023-10-30 15:41:08 +0900

    [BOJ] Silver2 가장 긴 증가하는 부분 수열

--- baekjoon/javascript/11053.js
+++ baekjoon/javascript/11053.js
@@ -3,6 +3,24 @@ const stdin = (
     process.platform === "linux"
       ? require('fs').readFileSync("/dev/stdin").toString()
       : `_
+      10 20 10 30 20 50`).trim().split("\n").map(e => e.trim());
+const arr = stdin[1].split(" ").map(n => Number(n));
+const dp = new Array(arr.length).fill(0);
+dp[0] = 1;
+
+for (let i=1; i<arr.length; i++) {
+    for (let k=0; k<i; k++) {
+        if (arr[i] > arr[k] && dp[i] < dp[k]) dp[i] = dp[k];
+    }
+    dp[i] += 1;
+}
+console.log(Math.max(...dp));
+
+/* Brute-force 재귀 O(n^2)풀이
+const stdin = (
+    process.platform === "linux"
+      ? require('fs').readFileSync("/dev/stdin").toString()
+      : `_
         5 4 3 2 1 5 4 3 2 1 5 4 3 2 1 5 4 3 2 1 5`).trim().split("\n").map(e => e.trim());
 const arr = stdin[1].split(" ").map(n => Number(n));
 let max = -Infinity;
@@ -27,3 +45,4 @@ function solution(stack, idx) {
 
 solution([], 0);
 console.log(max);
+*/
